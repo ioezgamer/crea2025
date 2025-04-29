dde8b26c16d5e79e187a8f59f357bb6ccf9711ad
ioezgamer <jo18mar@gmai.com>
2025-04-29 11:15:13 -0600

    SQL

---
+++ app/Console/Commands/EjecutarSQLParticipantes.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class EjecutarSQLParticipantes extends Command
+{
+    protected $signature = 'sql:participantes';
+    protected $description = 'Ejecuta el archivo SQL con datos de participantes';
+
+    public function handle()
+    {
+        $path = database_path('scripts/participantes.sql');
+
+        if (!file_exists($path)) {
+            $this->error('El archivo participantes.sql no se encontró.');
+            return 1;
+        }
+
+        $sql = file_get_contents($path);
+
+        try {
+            DB::unprepared($sql);
+            $this->info('Datos insertados correctamente desde el archivo SQL.');
+        } catch (\Exception $e) {
+            $this->error('Error al ejecutar el SQL: ' . $e->getMessage());
+        }
+
+        return 0;
+    }
+}
