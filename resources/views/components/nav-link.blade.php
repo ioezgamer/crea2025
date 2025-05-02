aa8947cf47b6f204f57d4b268a708cbcd6e6bb73
ioezgamer <jo18mar@gmai.com>
2025-05-02 14:32:25 -0600

    Sidebar

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,10 +2,13 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'flex items-center space-x-3 text-gray-900 bg-indigo-50 rounded-md px-3 py-2 font-medium'
-            : 'flex items-center space-x-3 text-gray-700 hover:bg-gray-100 rounded-md px-3 py-2';
+            ? 'flex items-center justify-between space-x-3 text-gray-900 bg-indigo-50 rounded-md px-3 py-2 font-medium'
+            : 'flex items-center justify-between space-x-3 text-gray-700 hover:bg-gray-100 rounded-md px-3 py-2';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
+    <div class="flex items-center space-x-3">
+        {{ $slot }}
+    </div>
+    <span class="text-xs text-gray-500">{{ $attributes->get('count') }}</span>
 </a>
