decf5f7d41f1031bf7c694727a255cc08cd5db30
ioezgamer <jo18mar@gmai.com>
2025-05-05 09:41:59 -0600

    Barra arriba

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,13 +2,13 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'flex items-center space-x-3 text-white bg-indigo-800 bg-opacity-20 rounded-lg px-3 py-2 font-medium'
-            : 'flex items-center space-x-3 text-gray-300 hover:bg-gray-700 rounded-lg px-3 py-2 transition-colors duration-200';
+            ? 'flex items-center space-x-1 text-white bg-indigo-600 bg-opacity-75 rounded-full px-2 py-1 font-medium'
+            : 'flex items-center space-x-1 text-gray-300 hover:bg-gray-700 hover:text-white rounded-full px-2 py-1 transition-colors duration-200';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
-    <div class="flex items-center space-x-3">
+    <div class="flex items-center space-x-1">
         {{ $slot }}
     </div>
-    <span class="text-xs bg-blue-600 rounded-full px-2 py-1">{{ $attributes->get('count') }}</span>
+    <span class="text-xs bg-blue-600 rounded-full px-1 py-0.5">{{ $attributes->get('count') }}</span>
 </a>
