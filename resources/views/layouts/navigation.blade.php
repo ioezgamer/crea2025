3118d0aa869bc7d7325a6433f5d1b2e5a62b1eb4
ioezgamer <jo18mar@gmai.com>
2025-05-06 10:53:04 -0600

    Cambios en el navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,85 +1,85 @@
 <nav x-data="{ open: false }" 
      class="bg-gray-900 text-white fixed top-0 left-0 right-0 z-50 shadow-md transition-all duration-300 ease-in-out"
      aria-label="Navegación Principal">
-    <div class="container mx-auto px-4 py-2 flex items-center justify-between">
+     <div class="container mx-auto px-4 py-2 flex items-center justify-between">
         <!-- Logo -->
-        <a href="{{ route('dashboard') }}" class="flex items-center space-x-3">
-            <x-application-logo class="h-8 w-auto fill-current text-white" />
-            <span class="text-lg font-bold">SistemaCREA</span>
+        <a href="{{ route('dashboard') }}" class="flex items-center space-x-2">
+            <x-application-logo class="h-6 w-full fill-current text-white" />
+            <span class="text-sm font-semibold">SistemaCREA</span>
         </a>
 
         <!-- Enlaces de Navegación -->
         <div class="hidden md:flex items-center space-x-6">
             <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" :count="$totalParticipants">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-1">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                     </svg>
-                    <span class="text-sm">Total Inscritos</span>
+                    <span class="text-xs">Total Inscritos</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('programas')" :active="request()->routeIs('programas')" :count="$activeProgramsCount">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-1">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
                     </svg>
-                    <span class="text-sm">Programas Activos</span>
+                    <span class="text-xs">Programas Activos</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('participante.index')" :active="request()->routeIs('participante')" :count="$meetingPlacesCount">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-1">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
-                    <span class="text-sm">Lugares de Encuentro</span>
+                    <span class="text-xs">Lugares de Encuentro</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('asistencia.create')" :active="request()->routeIs('asistencia')">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-1">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01m-.01 4h.01"></path>
                     </svg>
-                    <span class="text-sm">Asistencia</span>
+                    <span class="text-xs">Asistencia</span>
                 </div>
             </x-nav-link>
 
-            <x-nav-link :href="route('asistencia.reporte')" :active="request()->routeIs('asistencia')">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+            <x-nav-link :href="route('asistencia.reporte')" :active="request()->routeIs('asistencia')" >
+                <div class="flex items-center space-x-1">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                     </svg>
-                    <span class="text-sm">Reportes Asistencia</span>
+                    <span class="text-xs">Reportes Asistencia</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('tutores')" :active="request()->routeIs('tutores')" :count="$tutorsCount">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-1">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
                     </svg>
-                    <span class="text-sm">Tutores</span>
+                    <span class="text-xs">Tutores</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('tutores_participantes')" :active="request()->routeIs('tutores_participantes')" :count="$tutorsParticipantsCount">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-1">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
-                    <span class="text-sm">Tutores y Participantes</span>
+                    <span class="text-xs">Tutores y Participantes</span>
                 </div>
             </x-nav-link>
 
             @can('manage-roles')
                 <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.index')" :count="$rolesCount">
-                    <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                    <div class="flex items-center space-x-1">
                         <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0-1.104-.896-2-2-2s-2 .896-2 2c0 .738.402 1.376 1 1.723V15a1 1 0 001 1h2a1 1 0 001-1v-2.277c.598-.347 1-.985 1-1.723zm8 0c0-1.104-.896-2-2-2s-2 .896-2 2c0 .738.402 1.376 1 1.723V15a1 1 0 001 1h2a1 1 0 001-1v-2.277c.598-.347 1-.985 1-1.723zM6 3a3 3 0 00-3 3v12a3 3 0 003 3h12a3 3 0 003-3V6a3 3 0 00-3-3H6z"></path>
                         </svg>
-                        <span class="text-sm">Roles</span>
+                        <span class="text-xs">Roles</span>
                     </div>
                 </x-nav-link>
             @endcan
@@ -90,9 +90,9 @@
             <!-- Botón de menú móvil -->
             <button x-show="window.innerWidth < 768" 
                     @click="open = !open" 
-                    class="p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-white"
+                    class="p-1 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-white"
                     aria-label="Alternar Menú Móvil">
-                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <svg class="h-5 w-5" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                     <path x-show="!open" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     <path x-show="open" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
@@ -103,7 +103,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center space-x-2 text-gray-300 hover:bg-gray-700 rounded-full p-1 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-white">
-                            <span class="text-sm font-medium">{{ Auth::user()->name }}</span>
+                            <span class="text-xs font-medium">{{ Auth::user()->name }}</span>
                             <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                             </svg>
@@ -143,75 +143,76 @@
          class="md:hidden bg-gray-800 text-white">
         <div class="flex flex-col space-y-2 p-4">
             <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" :count="$totalParticipants">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-2">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                     </svg>
-                    <span class="text-sm">Total Inscritos</span>
+                    <span>Total Inscritos</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('programas')" :active="request()->routeIs('programas')" :count="$activeProgramsCount">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-2">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
                     </svg>
-                    <span class="text-sm">Programas Activos</span>
+                    <span>Programas Activos</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('participante.index')" :active="request()->routeIs('participante')" :count="$meetingPlacesCount">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-2">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
-                    <span class="text-sm">Lugares de Encuentro</span>
+                    <span>Lugares de Encuentro</span>
                 </div>
             </x-nav-link>
 
-            <x-nav-link :href="route('asistencia.create')" :active="request()->routeIs('asistencia')">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+            <x-nav-link :href="route('asistencia.create')" :active="request()->routeIs('asistencia')" >
+                <div class="flex items-center space-x-2">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01m-.01 4h.01"></path>
                     </svg>
-                    <span class="text-sm">Asistencia</span>
+                    <span>Asistencia</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('asistencia.reporte')" :active="request()->routeIs('asistencia')">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-2">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                     </svg>
-                    <span class="text-sm">Reportes Asistencia</span>
+                    <span>Reportes Asistencia</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('tutores')" :active="request()->routeIs('tutores')" :count="$tutorsCount">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-2">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
                     </svg>
-                    <span class="text-sm">Tutores</span>
+                    <span>Tutores</span>
                 </div>
             </x-nav-link>
 
             <x-nav-link :href="route('tutores_participantes')" :active="request()->routeIs('tutores_participantes')" :count="$tutorsParticipantsCount">
-                <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                <div class="flex items-center space-x-2">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
-                    <span class="text-sm">Tutores y Participantes</span>
+                    <span>Tutores y Participantes</span>
                 </div>
             </x-nav-link>
 
             @can('manage-roles')
                 <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.index')" :count="$rolesCount">
-                    <div class="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md transition-colors">
+                    <div class="flex items-center space-x-2">
                         <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0-1.104-.896-2-2-2s-2 .896-2 2c0 .738.402 1.376 1 1.723V15a1 1 0 001 1h2a1 1 0 001-1v-2.277c.598-.347 1-.985 1-1.723zm8 0c0-1.104-.896-2-2-2s-2 .896-2 2c0 .738.402 1.376 1 1.723V15a1 1 0 001 1h2a1 1 0 001-1v-2.277c.598-.347 1-.985 1-1.723zM6 3a3 3 0 00-3 3v12a3 3 0 003 3h12a3 3 0 003-3V6a3 3 0 00-3-3H6z"></path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0-1.104-.896-2-2-2s-2 .896-2 2c0 .738.402 1.376 1 1.723V15a1 1 0 001 1h2a1 1 0 001-1v-2.277c.598-.347 1-.985 1-1.723zm8 0c0-1.104-.896-2-2-2s-2 .896-2 2c0 .738.402 1.376 1 1.723V15a1 1 0 001 1h2a1 1 0 001-1v-2.277c.598 Rates and Contracts
+-.347 1-.985 1-1.723zM6 3a3 3 0 00-3 3v12a3 3 0 003 3h12a3 3 0 003-3V6a3 3 0 00-3-3H6z"></path>
                         </svg>
-                        <span class="text-sm">Roles</span>
+                        <span>Roles</span>
                     </div>
                 </x-nav-link>
             @endcan
@@ -250,3 +251,6 @@
         </div>
     </div>
 </nav>
+
+<!-- Espaciador para evitar que el contenido quede detrás de la barra -->
+<div class="h-12 md:h-12"></div>
