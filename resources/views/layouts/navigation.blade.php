ebffb0cd5cc240786fe92a005640631fcf831fff
ioezgamer <jo18mar@gmai.com>
2025-05-20 08:08:23 -0600

    Detalles t/p

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -55,12 +55,12 @@
                     @endcan
                     
                     {{-- El enlace 'tutores_participantes' podría ser redundante si 'tutores' ya cubre esto, o podría ser una vista específica --}}
-                    {{-- <x-nav-link :href="route('tutores_participantes')" :active="request()->routeIs('tutores_participantes')">
+                    <x-nav-link :href="route('tutores_participantes')" :active="request()->routeIs('tutores_participantes')">
                         <div class="flex items-center space-x-1.5">
                             <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                             <span class="text-xs font-medium">Detalles tutores</span>
                         </div>
-                    </x-nav-link> --}}
+                    </x-nav-link> 
 
                     @can('manage-roles')
                         <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.index')" :count="$rolesCount ?? 0">
